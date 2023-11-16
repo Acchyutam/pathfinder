d4e2777e1eee959bc5e25fcb99fbf2f08ec53681
Acchyutam <143492390+Acchyutam@users.noreply.github.com>
2023-11-16 23:01:32 +0530

    Update pathfinder.cpp

--- Pathfinder/pathfinder.cpp
+++ Pathfinder/pathfinder.cpp
@@ -11,27 +11,25 @@ using namespace std;
 #define RESET   "\033[0m" // Color code reset to default
 #define GREEN   "\033[32m" // Color code set to Green
 #define RED     "\033[31m" // Color code set to red
-// Blocks of the maze
-struct block{
+// MazeNodes of the maze
+struct MazeNode{
     int weight;
-    vector<block*> neighbours;
+    vector<MazeNode*> neighbours;
     int shortestDistance;
-    block *prevBlock;
+    MazeNode *prevMazeNode;
 };
 
-/* All the functions before the "setNeighbours" function are used in the implementation of B-tree. */
 
 struct btNode{
-    block *keys[MAX + 1];         // Array of pointer to blocks stored in the B-tree node (aka keys of the node)
-    int count;                     // Number of blocks in a B-tree node
-    btNode *children[MAX + 1];  // Array of pointer to children blocks
+    MazeNode *keys[MAX + 1];         
+    int count;                     
+    btNode *children[MAX + 1];  
 };
 
-// Creating the null root of the tree
 btNode *root = nullptr;
 
-btNode *createNewNode(block* key, btNode *child){
-    // Creates a new B-tree node and returns the pointer to it
+btNode *createNewNode(MazeNode* key, btNode *child){
+
     btNode *newNode = new btNode;
     newNode->keys[1] = key;
     newNode->count = 1;
@@ -40,8 +38,7 @@ btNode *createNewNode(block* key, btNode *child){
     return newNode;
 }
 
-void insertKeyIntoNode(block* key, int position, btNode *node, btNode *child) {
-    // Adds the key to the node in a way that it remains sorted
+void insertKeyIntoNode(MazeNode* key, int position, btNode *node, btNode *child) {
     int i;
     for ( i = node->count; i > position; i-- ) {
         node->keys[i + 1] = node->keys[i];
@@ -53,8 +50,7 @@ void insertKeyIntoNode(block* key, int position, btNode *node, btNode *child) {
     node->count++;
 }
 
-void splitNode(block *key, block **prevKey, int position, btNode *node, btNode *child, btNode **newNode) {
-    // Splitting the node for insertion
+void splitNode(MazeNode *key, MazeNode **prevKey, int position, btNode *node, btNode *child, btNode **newNode) {
     int splitPoint;
     int median = MIN;
 
@@ -66,7 +62,6 @@ void splitNode(block *key, block **prevKey, int position, btNode *node, btNode *
     *newNode = new btNode;
 
     while (splitPoint <= MAX) {
-        //  Moving all the right most nodes to a new node
         (*newNode)->keys[splitPoint - median]     = node->keys[splitPoint];
         (*newNode)->children[splitPoint - median] = node->children[splitPoint];
         splitPoint++;
@@ -76,12 +71,10 @@ void splitNode(block *key, block **prevKey, int position, btNode *node, btNode *
     (*newNode)->count = MAX - median;
 
     if (position <= MIN) {
-        // Add key to left part
         insertKeyIntoNode(key, position, node, child);
     }
 
     else {
-        // Add key to right part
         insertKeyIntoNode(key, position - median, *newNode, child);
     }
 
@@ -91,41 +84,33 @@ void splitNode(block *key, block **prevKey, int position, btNode *node, btNode *
     node->count--;
 }
 
-bool AddNewKey(block *newKey, block **prevValue, btNode *node, btNode **child) {
-    // Adds the new key into the node
+bool AddNewKey(MazeNode *newKey, MazeNode **prevValue, btNode *node, btNode **child) {
 
     int position;
 
     if(!node) {
-        // There are no more nodes
         *prevValue = newKey;
         *child = NULL;
         return true;
     }
 
     if( newKey->shortestDistance < node->keys[1]->shortestDistance ) {
-        // Key is smaller than the first key in the node
         position = 0;
     }
 
     else {
-        // Go backwards until finding the new node's place in the node
         position = node->count;
         while(newKey->shortestDistance < node->keys[position]->shortestDistance && position > 1)
             position--;
     }
 
     if ( AddNewKey(newKey, prevValue, node->children[position], child) ) {
-        // If the node has no children at that position
-        // so the new key should be added to the node
 
         if (node->count < MAX) {
-            // Node is not full -> add the key to this node
             insertKeyIntoNode(*prevValue, position, node, *child);
         }
 
         else {
-            // Node is full -> split the node
             splitNode(*prevValue, prevValue, position, node, *child, child);
             return true;
         }
@@ -134,23 +119,19 @@ bool AddNewKey(block *newKey, block **prevValue, btNode *node, btNode **child) {
     return false;
 }
 
-void insert(block *newBlock) {
-    // Adds a new Block to the B-tree
+void insert(MazeNode *newMazeNode) {
 
     bool newRoot;
-    block *i;
+    MazeNode *i;
     btNode *child;
 
-    newRoot = AddNewKey(newBlock, &i, root, &child);
+    newRoot = AddNewKey(newMazeNode, &i, root, &child);
 
     if(newRoot)
-        // Creating new root
         root = createNewNode(i, child);
 }
 
 void removeKey(btNode *node, int position) {
-    // Deletes the value from the node using its position
-    // while keeping the node sorted
 
     for(int i= position + 1; i <= node->count; i++){
         node->keys[i - 1]     = node->keys[i];
@@ -160,10 +141,6 @@ void removeKey(btNode *node, int position) {
 }
 
 void shiftKeyToChild(btNode *node, int position, char leftRight) {
-    //Shifts block from parent to right child if 'r'
-    // and to left child if 'l'
-
-    // Does right shift
     if(leftRight == 'r'){
         btNode *x = node->children[position];
         int j = x->count;
@@ -185,7 +162,6 @@ void shiftKeyToChild(btNode *node, int position, char leftRight) {
         return;
     }
 
-    // Does left shift
     if(leftRight == 'l'){
         int j = 1;
         btNode *x = node->children[position - 1];
@@ -209,7 +185,6 @@ void shiftKeyToChild(btNode *node, int position, char leftRight) {
 }
 
 void mergeNodes(btNode *node, int position) {
-    // Merges two brothers that have less than MIN keys
 
     int j;
 
@@ -236,7 +211,6 @@ void mergeNodes(btNode *node, int position) {
 }
 
 void fixNode(btNode *node, int position) {
-    // Fixes the given node
 
     if (!position) {
         if (node->children[1]->count > MIN) {
@@ -275,7 +249,6 @@ void fixNode(btNode *node, int position) {
 }
 
 void replaceSuccessor(btNode *deadNode, int position) {
-    // Replaces the successor for the node to be deleted (aka deadNode)
 
     btNode *dummy = deadNode->children[position];
 
@@ -286,32 +259,28 @@ void replaceSuccessor(btNode *deadNode, int position) {
 
 }
 
-bool removeKey(block *deadBlock,btNode *node) {
-    // Removes the deadBlock from the node
+bool removeKey(MazeNode *deadMazeNode,btNode *node) {
 
     bool flag = false;
     int position;
 
-    if (node) {   // If root is not empty
+    if (node) { 
 
-        if (deadBlock->shortestDistance < node->keys[1]->shortestDistance) {
-            // Key is smaller than the first key in the node
+        if (deadMazeNode->shortestDistance < node->keys[1]->shortestDistance) {
             position = 0;
             flag = false;
         }
 
         else {
-            // Go backwards until finding right position
             position = node->count;
-            while((deadBlock->shortestDistance  <= node->keys[position]->shortestDistance) &&
-                (deadBlock  != node->keys[position])  &&
+            while((deadMazeNode->shortestDistance  <= node->keys[position]->shortestDistance) &&
+                (deadMazeNode  != node->keys[position])  &&
                 (position > 1))
 
                  position--;
 
-            if ((deadBlock  == node->keys[position]) &&
-                (deadBlock->shortestDistance  == node->keys[position]->shortestDistance)) {
-            // To avoid deleting the wrong duplicate
+            if ((deadMazeNode  == node->keys[position]) &&
+                (deadMazeNode->shortestDistance  == node->keys[position]->shortestDistance)) {
                 flag = true;
             }
             else {
@@ -320,7 +289,6 @@ bool removeKey(block *deadBlock,btNode *node) {
         }
 
         if (flag) {
-            // The brother node does not have enough keys to lend
             if (node->children[position - 1]) {
                 replaceSuccessor(node, position);
                 flag = removeKey(node->keys[position], node->children[position]);
@@ -331,12 +299,11 @@ bool removeKey(block *deadBlock,btNode *node) {
         }
 
         else {
-            flag = removeKey(deadBlock, node->children[position]);
+            flag = removeKey(deadMazeNode, node->children[position]);
         }
 
         if (node->children[position] &&
            (node->children[position]->count < MIN)) {
-            // The children become less than MIN so the node should be fixed
             fixNode(node, position);
         }
     }
@@ -344,15 +311,13 @@ bool removeKey(block *deadBlock,btNode *node) {
     return flag;
 }
 
-void remove(block *deadBlock, btNode *node) {
-    // Takes a pointer to a block in the B-tree and deletes it
+void remove(MazeNode *deadMazeNode, btNode *node) {
 
     btNode *temp;
 
-    if(!removeKey(deadBlock, node))
+    if(!removeKey(deadMazeNode, node))
         return;
 
-    // node becomes empty so we free up the memory
     if( node->count == 0 ){
             temp = node;
             node = node->children[0];
@@ -361,15 +326,13 @@ void remove(block *deadBlock, btNode *node) {
 
     root = node;
 }
-btNode *removeAndGetNewRoot(block *deadBlock, btNode *node) {
-    // Removes the deadBlock from the node and returns the updated root
+btNode *removeAndGetNewRoot(MazeNode *deadMazeNode, btNode *node) {
 
     btNode *temp;
 
-    if (!removeKey(deadBlock, node))
+    if (!removeKey(deadMazeNode, node))
         return node;
 
-    // node becomes empty, so we free up the memory
     if (node->count == 0) {
         temp = node;
         node = node->children[0];
@@ -379,9 +342,8 @@ btNode *removeAndGetNewRoot(block *deadBlock, btNode *node) {
     return node;
 }
 
-block *popMin(btNode *&root) {
-    // Deletes the minimum key and returns it
-    block *min;
+MazeNode *popMin(btNode *&root) {
+    MazeNode *min;
     btNode *ptr = root;
 
     while (ptr->children[0] != nullptr) {
@@ -396,25 +358,21 @@ block *popMin(btNode *&root) {
 
 
 bool isEmpty(btNode *root) {
-    // Returns true if the B-tree is empty
     if(root == nullptr)
         return true;
     else
         return false;
 }
 
-void setNeighbours(vector<vector<block*>> &maze, int row, int col) {
-    // Creates a vector of pointers to the neighbours of each block and
-    // sets it to maze[i]->neighbours for node i
-
+void setNeighbours(vector<vector<MazeNode*>> &maze, int row, int col) {
     int weight;
-    vector<block*> neighbours;
+    vector<MazeNode*> neighbours;
 
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             weight = maze[i][j]->weight;
 
-            if (weight > 0) { // if not a dead-end
+            if (weight > 0) { 
 
                 if ((j - 1 >= 0) && (maze[i][j - 1]->weight > 0)) {
                     neighbours.push_back(maze[i][j - 1]); // left neighbour
@@ -436,48 +394,39 @@ void setNeighbours(vector<vector<block*>> &maze, int row, int col) {
 }
 
 
-void bTreeDijkstra(vector<vector<block*>> &maze, block *source, int row, int col){
-    /* Takes a 2D vector of blocks and sets the block->shortestDistance and
-       block->prevBlock using Dijkstra algorithm that is implemented using a B-tree
-       instead of a priority queue. */
+void bTreeDijkstra(vector<vector<MazeNode*>> &maze, MazeNode *source, int row, int col){
 
-    block *currentBlock = nullptr;
+    MazeNode *currentMazeNode = nullptr;
     int newDist = 0;
 
     source->shortestDistance = 0;
 
-    insert(source); // Insert into B-tree
+    insert(source);
 
     while( !isEmpty(root) ){
-        currentBlock = popMin(root);
+        currentMazeNode = popMin(root);
         
 
-        for( auto neighbour : currentBlock->neighbours ){
-            newDist = currentBlock->shortestDistance + neighbour->weight;
+        for( auto neighbour : currentMazeNode->neighbours ){
+            newDist = currentMazeNode->shortestDistance + neighbour->weight;
 
             if( newDist < neighbour->shortestDistance ){
-                // a shorter distance is found
                 remove(neighbour, root);
                 neighbour->shortestDistance = newDist;
-                neighbour->prevBlock = currentBlock;
+                neighbour->prevMazeNode = currentMazeNode;
                 insert(neighbour);
-                // Note: We are removing and inserting again instead of changing
-               //        the key so we don't mess up the search tree properties.
                 }
             }
         }
     }
 
-void solveMaze(vector<vector<block*>> &maze, block *source, int row, int col) {
+void solveMaze(vector<vector<MazeNode*>> &maze, MazeNode *source, int row, int col) {
     int steps = 0 ;
-    // After setting the shortest distance and previous block for each node
-    // this helper function changes the nodes on the shortest path to -1
-
     bTreeDijkstra(maze, source, row, col);
 
     int i = row-1;
     int j = col-1 ;
-    block *goal;
+    MazeNode *goal;
 
     while( maze[i][j]->weight==0 ){
         j--;
@@ -485,11 +434,11 @@ void solveMaze(vector<vector<block*>> &maze, block *source, int row, int col) {
 
     goal = maze[i][j];
 
-    block *ptr = goal;
+    MazeNode *ptr = goal;
 
-    while( ptr->prevBlock != nullptr ){
+    while( ptr->prevMazeNode != nullptr ){
         ptr->weight = -1;
-        ptr = ptr->prevBlock;
+        ptr = ptr->prevMazeNode;
         steps++;
     }
 
@@ -497,7 +446,7 @@ void solveMaze(vector<vector<block*>> &maze, block *source, int row, int col) {
     cout << GREEN << "\n\n\t\t\tPath cost: " << steps << " steps" << RESET << endl;
 }
 
-void printInputMaze(vector<vector<block*>> &maze, int row, int col) {
+void printInputMaze(vector<vector<MazeNode*>> &maze, int row, int col) {
     // Find the maximum number of digits in the maze values
     int maxDigits = 0;
     for (int i = 0; i < row; i++) {
@@ -531,7 +480,7 @@ void printInputMaze(vector<vector<block*>> &maze, int row, int col) {
 }
 
 
-void printSolvedMaze(vector<vector<block*>> &maze, int row, int col) {
+void printSolvedMaze(vector<vector<MazeNode*>> &maze, int row, int col) {
     // Find the maximum number of digits in the maze values
     int maxDigits = 0;
     for (int i = 0; i < row; i++) {
@@ -571,30 +520,30 @@ void printSolvedMaze(vector<vector<block*>> &maze, int row, int col) {
 
 
 // Function to take input for the maze
-void takeInput(vector<vector<block*>>& maze, block*& source, int& row, int& col, ifstream& inputFile) {
+void takeInput(vector<vector<MazeNode*>>& maze, MazeNode*& source, int& row, int& col, ifstream& inputFile) {
     inputFile >> row;
     inputFile >> col;
 
-    vector<block*> tempRow;
+    vector<MazeNode*> tempRow;
 
-    // Inputs the plain maze into a 2D vector of pointers to blocks
+    // Inputs the plain maze into a 2D vector of pointers to MazeNodes
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             int weight;
             inputFile >> weight;
 
-            block* newBlock = new block;
-            newBlock->weight = weight;
-            newBlock->prevBlock = nullptr;
-            newBlock->shortestDistance = 100000000;
-            newBlock->neighbours = {};
-            tempRow.push_back(newBlock);
+            MazeNode* newMazeNode = new MazeNode;
+            newMazeNode->weight = weight;
+            newMazeNode->prevMazeNode = nullptr;
+            newMazeNode->shortestDistance = 100000000;
+            newMazeNode->neighbours = {};
+            tempRow.push_back(newMazeNode);
         }
         maze.push_back(tempRow);
         tempRow.clear();
     }
 
-    // Finding the source (aka starting) block
+    // Finding the source (aka starting) MazeNode
     for (int i = 0; i < col; i++) {
         if (maze[0][i]->weight == 1) {
             source = maze[0][i];
@@ -606,9 +555,9 @@ void takeInput(vector<vector<block*>>& maze, block*& source, int& row, int& col,
 
 int main() {
     int col, row;
-    block* source;
+    MazeNode* source;
     
-    vector<vector<block*>> maze;
+    vector<vector<MazeNode*>> maze;
     
     // Create an input stream for reading from a text file
     ifstream inputFile("maze.txt"); // Replace "maze.txt" with your file path
